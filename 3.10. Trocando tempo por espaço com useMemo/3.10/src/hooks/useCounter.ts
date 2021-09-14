c1bf738fe2e2b3c1e6feeac9768aa373ddd4aeec
leandrosimonato <leandrosimonato@gmail.com>
2021-09-13 21:25:51 -0300

    Trocando tempo por espaço com useMemo.

---
+++ 3.10. Trocando tempo por espaço com useMemo/3.10/src/hooks/useCounter.ts
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+
+export default function useCounter (initialNumber = 1) {
+  const [number, setNumber] = useState(initialNumber)
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      setNumber(number + 1)
+    }, 5000)
+    
+  }, [])
+
+  return number
+}
